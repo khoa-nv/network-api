a43a27b91cd652ba4d2441104740ae36c8ec631a
Khoa Nguyen <khoanv.me@gmail.com>
2025-02-21 19:08:57 +0700

    update

--- clients/cli/src/flops.rs
+++ clients/cli/src/flops.rs
@@ -8,7 +8,6 @@ const OPERATIONS_PER_ITERATION: u64 = 4; // sin, add, multiply, divide
 
 pub fn measure_flops() -> f32 {
     let start = Instant::now();
-
     let total_flops: f64 = 2147483647;
 
     let duration = start.elapsed();
