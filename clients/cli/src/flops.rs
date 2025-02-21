ee7832c49966ba118f209b85429fe003c8e4f5e6
Khoa Nguyen <khoanv.me@gmail.com>
2025-02-21 19:06:59 +0700

    update

--- clients/cli/src/flops.rs
+++ clients/cli/src/flops.rs
@@ -3,7 +3,16 @@
 use rayon::prelude::*;
 use std::time::Instant;
 
+const NTESTS: u64 = 1_000_000;
+const OPERATIONS_PER_ITERATION: u64 = 4; // sin, add, multiply, divide
+
 pub fn measure_flops() -> f32 {
-    let flops = 2147483647;
+    let start = Instant::now();
+
+    let total_flops: f64 = 2147483647;
+
+    let duration = start.elapsed();
+
+    let flops = total_flops;
     flops as f32
 }
