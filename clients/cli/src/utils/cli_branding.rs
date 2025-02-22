fbf03aaf10a1b9262851978e2a91f8da788e32d7
Khoa Nguyen <khoanv.me@gmail.com>
2025-02-22 22:34:44 +0700

    update

--- clients/cli/src/utils/cli_branding.rs
+++ clients/cli/src/utils/cli_branding.rs
@@ -1,52 +1,5 @@
 use colored::Colorize;
 
-pub const LOGO_NAME: &str = r#"
-                                     ))[++                                     
-                                 /)()))[+++++_                                 
-                              |(())))))[++++++++<                              
-                           )())))))))))[++++++++++++                           
-                        )()))))))))))))[+++++++++++++~+                        
-                     )())))))))))))))))[++++++++++++++++++                     
-                 ()()))))))))))))))))))[++++++++++++++++++++++                 
-              |))))))))))))))))))))))))[++++++++++++++++++++++~++              
-            t))))))))))))))))))))))))))[++++++++++++++++++++++++++<            
-            bbZv())))))))))))))))))))))[+++++++++++++++++++++++i:^^            
-            bbbbbpc/)))))))))))))))))))[++++++++++++++++++~<!"^^^^^            
-            bbbbbbbbd0|))))))))))))))))[++++++++++++++++~;^^^^^^^^^            
-            bbbbbbbbbbbdQr)))))))))))))[+++++++++++++>;^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbZr|)))))))))[++++++++~~>,^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbbbbpX())))))[++++++~l"^^^^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbbbbbbbpYt)))[+++<I"^^^^^^^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbbbbbbbbbbbQt[<;^^^^^^^^^^^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbbbbbbbbbbbdm1'`^^^^^^^^^^^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbbbbbbbbpwZZZ1....`^^^^^^^^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbbbbbpmZZZZZZ1.......`^^^^^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbbbbdwmmZZZZZZZZ1..........'`^^^^^^^^^^^^^^^            
-            bbbbbbbbbbbbdwZZZZZZZZZZZZZ1.............'`^^^^^^^^^^^^            
-            bbbbbbbbbdmZZZZZZZZZZZZZZZZ1.................`^^^^^^^^^            
-            bbbbbbpmmZZZZZZZZZZZZZZZZZZ1....................'^^^^^^            
-            bbdpmZZZZZZZZZZZZZZZZZZZZZZ1.......................'`^^            
-            mZZZZZZZZZZZZZZZZZZZZZZZZZZ1...........................            
-              mmZZZZZZZZZZZZZZZZZZZZZZZ1.........................              
-                 mmZZZZZZZZZZZZZZZZZZZZ1................... .                  
-                     wmZZZZZZZZZZZZZZZZ1.................                      
-                       mmmZZZZZZZZZZZZZ1..............                         
-                           mmZZZZZZZZZZ1............                           
-                              wmZZZZZZZ1.......                                
-                                 wmZZZZ1.....                                  
-                                     mZ1.                                      
-                                                                               
-                                                                               
-                                                                               
-00       00        0000000000     00       000     00        00       000000000
-0000     00        00              000   000       00        00       00      0
-00000    00        00                00 000        00        00       00       
-00  000  00        000000000          000          00        00        0000000 
-00    00 00        00                00 000        00        00              00
-00     0000        00              000   000        00      00       00      00
-00      000        0000000000     00       000       00000000         000000000
-"#;
-
 pub fn print_banner() {
     println!("{}", LOGO_NAME.bright_cyan());
     println!(
